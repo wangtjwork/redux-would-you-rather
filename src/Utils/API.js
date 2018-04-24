aa494c030d0f9ecaccf4370282f4363bfd800a25
Tianjian Wang <wangtjwork@gmail.com>
2018-04-24 13:36:57 -0500

    feat: Add getInitialData API to APIs

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,7 +1,17 @@
 import { _getUsers, _getQuestions, _saveQuestion, _saveQuestionAnswer } from './_DATA';
 
+export function getInitialData() {
+  return Promise.all([
+    _getUsers(),
+    _getQuestions(),
+  ]).then(([users, questions]) => ({
+    users,
+    questions,
+  }));
+}
+
 export function getUsers() {
-  return _getUsers();
+  return _getUsers().then((users) => ({ users }));
 }
 
 export function getQuestions() {
