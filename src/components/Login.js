cdfd9922c5125c239b4ec32e1ef40fa46abb4e03
Tianjian Wang <wangtjwork@gmail.com>
2018-04-24 22:30:49 -0500

    feat: Add controlled state and change to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import setAuthedUser from '../actions/authedUser';
+import { connect } from 'react-redux';
 
 class Login extends Component {
+  state = {
+    selectedUser: null
+  }
+
+  handleChange = (e) => {
+    const id = e.target.value;
+
+    this.setState({
+      selectedUser: id,
+    });
+    console.log(id);
+  }
+
   render() {
+    const { users } = this.props;
+
     return (
-      <div>My Login</div>
+      <select onChange={this.handleChange}>
+        {
+          users.map((user) => (
+            <option key={user.id} value={user.id}>{user.name}</option>
+          ))
+        }
+      </select>
     )
   }
 }
 
-export default Login;
+const mapStateToProps = ({ users }) => {
+  return {
+    users: Object.keys(users).map((id) => ({
+      id,
+      name: users[id].name
+    }))
+  }
+}
+
+export default connect(mapStateToProps)(Login);
