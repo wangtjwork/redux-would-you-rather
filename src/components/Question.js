fd07acb37752f7a1bb154a75e65c719e83d0ce23
Tianjian Wang <wangtjwork@gmail.com>
2018-04-25 21:55:12 -0500

    feat: Add props to Question and UI rendering based on props

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class Question extends React.Component {
+class Question extends Component {
   render() {
+    const { curUser, question } = this.props;
+    const hasAnswered = curUser.answers.hasOwnProperty(question.id);
+    const totalVotes = question.optionOne.votes.length
+      + question.optionTwo.votes.length;
+
+    const optionOnePercentage = percentCalculation(question.optionOne.votes.length, totalVotes);
+    const optionTwoPercentage = 100 - optionOnePercentage;
+
     return (
       <div>
-        <h2>Would you rather...</h2>
+        <h2>{curUser.name}</h2>
+        <h3>Would you rather...</h3>
+        <div>
+          <span>
+            {hasAnswered === false
+              ? <button>{question.optionOne.text}</button>
+              : question.optionOne.text
+            }
+            {hasAnswered && (totalVotes === 0
+              ? 'No Votes Yet'
+              : optionOnePercentage + '%'
+            )}
+          </span>
+          <span> Or </span>
+          <span>
+            {hasAnswered === false
+              ? <button>{question.optionTwo.text}</button>
+              : question.optionTwo.text
+            }
+            {hasAnswered && (totalVotes === 0
+              ? 'No Votes Yet'
+              : optionTwoPercentage + '%'
+            )}
+          </span>
+        </div>
       </div>
     )
   }
 }
 
-export default Question
+const percentCalculation = (num, total) => {
+  const percentage = Math.round(num * 100 / total);
+  return percentage;
+}
+
+const mapStateToProps = ({ users, questions, authedUser }) => {
+  return {
+    curUser: users[authedUser],
+    question: questions["8xf0y6ziyjabvozdd253nd"],
+  }
+}
+
+export default connect(mapStateToProps)(Question);
