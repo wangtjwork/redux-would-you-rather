78a805619a99dd157500f57fac405fb155d47f68
Tianjian Wang <wangtjwork@gmail.com>
2018-04-26 23:07:26 -0500

    feat: Add LeaderBoard component basic structure

---
+++ src/components/LeaderBoard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LeaderBoard extends Component {
+  render() {
+    return (
+      <div>LeaderBoard</div>
+    )
+  }
+}
+
+export default LeaderBoard;
