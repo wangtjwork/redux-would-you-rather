8ac3f3424821dbe52aaedcdce7c8e8468d287424
Tianjian Wang <wangtjwork@gmail.com>
2018-04-26 22:21:53 -0500

    feat: Make AddQuestion a controlled component, add two input fields

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 class AddQuestion extends Component {
+  state = {
+    optionOneText: '',
+    optionTwoText: ''
+  }
+
+  handleChangeText = (text, option) => {
+    console.log(text, option);
+    this.setState((prevState) => ({
+      ...prevState,
+      [option]: text,
+    }));
+  }
+
   render() {
     return (
       <div>
         <h2>Add Question</h2>
         <h3>Would you rather...</h3>
+        <input type='text' placeholder="Option One"
+          value={this.state.optionOneText}
+          onChange={(e) => this.handleChangeText(e.target.value, 'optionOneText')}
+        />
+        <input type='text' placeholder="Option Two"
+          value={this.state.optionTwoText}
+          onChange={(e) => this.handleChangeText(e.target.value, 'optionTwoText')}
+        />
       </div>
     )
   }
